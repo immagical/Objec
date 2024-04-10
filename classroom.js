64f9490eda2d8c4d7a2cb1bba55d54d1880f00c9
immagical <104602961+immagical@users.noreply.github.com>
2024-04-10 17:22:20 -0400

    Add files via upload

---
+++ classroom.js
@@ -0,0 +1,33 @@
+modelStatus = " "
+
+function preload() {
+    house = loadImage("fourthimage.jpeg"); 
+}
+function setup() {
+    canvas = createCanvas(600, 500); 
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Object"; 
+}
+
+function modelLoaded() {
+    console.log("Model Loaded"); 
+    modelStatus = "true"; 
+    objectDetector.detect(house, gotResults); 
+}
+
+function gotResults(error, results) {
+    if(error) {
+        console.error(error); 
+    }
+    if(results) {
+        console.log(results); 
+    }
+}
+
+function draw() {
+    image(house, 0, 0, 600, 500); 
+}
+
+function back() {
+    window.location = "index.html"; 
+}
